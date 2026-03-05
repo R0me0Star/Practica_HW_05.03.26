cccb2716c780c09cb9ba8cfba27fafec75a98a10
R0me0Star <eip_spb@mail.ru>
2026-03-05 14:54:29 +0300

    finished logic

--- main.cpp
+++ main.cpp
@@ -1,2 +1,32 @@
-main()
-{}
+template < class T > T *merge(const T *a, size_t sa, const T *b, size_t sb, T *c)
+{
+  const T *end_a = a + sa;
+  const T *end_b = b + sb;
+  T *current_c = c;
+
+  while (a != end_a && b != end_b) {
+    if (*a < *b) {
+      *current_c = *a;
+      ++a;
+    } else {
+      *current_c = *b;
+      ++b;
+    }
+
+    ++current_c;
+  }
+
+  while (a != end_a) {
+    *current_c = *a;
+    ++a;
+    ++current_c;
+  }
+
+  while (b != end_b) {
+    *current_c = *b;
+    ++b;
+    ++current_c;
+  }
+
+  return c;
+}
